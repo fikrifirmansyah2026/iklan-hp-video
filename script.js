836c5e8c3d86a607cbd5975b82c8381e572a611c
Fikri Firmansyah <fikrifir80@gmail.com>
2026-04-23 03:43:58 +0700

    Create script.js
    Add Initial Create script.js with project

---
+++ script.js
@@ -0,0 +1,12 @@
+const text = document.getElementById("text");
+const sub = document.getElementById("sub");
+
+setTimeout(() => {
+  text.innerText = "Bikin Website Mudah!";
+  sub.innerText = "Fikri Web Studio";
+}, 2000);
+
+setTimeout(() => {
+  text.innerText = "Mulai dari Sekarang";
+  sub.innerText = "Klik tombol di bawah";
+}, 4000);
